385dcb9db2dee1ec1c5c7bc60c7da8c6c03aba7a
thanhmin34 <79617357+thanhmin34@users.noreply.github.com>
2022-06-13 09:06:38 +0700

    create imagesUrls

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Footer from "../Footer";
 const Header = () => {
   return (
     <div className="w-full  bg-slate-700 text-[#fff] h-[60px] ">
       <div className="w-full max-w-[1280px] mx-auto  flex items-center justify-start gap-5 leading-[60px]">
         <Link className="px-4 " to="/">
           Trang chu
+        </Link>
+        <Link className="px-4 " to="/gio-hang">
+          Gio hang
         </Link>
         <Link className="px-4 " to="/gio-hang">
           Gio hang
---
+++ src/Constans/imagesUrls.js
@@ -0,0 +1,5 @@
+const imagesUrls = {
+  nameTest: "/assets/images/icon-fb.png",
+  Logo: "/assets/images/logo.png",
+  banner: "/assets/images/banner.png",
+};
