499a59842afe3178acf7442f35d070a9bfea8f05
thanhmin34 <79617357+thanhmin34@users.noreply.github.com>
2022-06-13 09:09:38 +0700

    minh

--- src/Constans/imagesUrls.js
+++ src/Constans/imagesUrls.js
@@ -2,4 +2,6 @@ const imagesUrls = {
   nameTest: "/assets/images/icon-fb.png",
   Logo: "/assets/images/logo.png",
   banner: "/assets/images/banner.png",
+  bannerTwo: "/assets/images/banner.png",
+  bannerTwo2: "acs",
 };
